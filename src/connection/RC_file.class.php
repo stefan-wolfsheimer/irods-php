a849fefe54c1c00603a47ae48c3eb4fa1a2da0b9
Lazlo Westerhof <l.r.westerhof@uu.nl>
2019-04-25 12:14:46 +0200

    Fix API call.

--- src/connection/RC_file.class.php
+++ src/connection/RC_file.class.php
@@ -396,7 +396,7 @@ trait RC_file {
      */
     public function fileSeek($l1desc, $offset, $whence = SEEK_SET) {
         $openedDataObjInp = new RP_OpenedDataObjInp($l1desc, 0, $offset, $whence);
-        $msg = new RODSMessage("RODS_API_REQ_T", $openedDataObjInp, $GLOBALS['PRODS_API_NUMS']['DATA_OBJ_SEEK_AN']);
+        $msg = new RODSMessage("RODS_API_REQ_T", $openedDataObjInp, $GLOBALS['PRODS_API_NUMS']['DATA_OBJ_LSEEK_AN']);
 
         fwrite($this->conn, $msg->pack()); // send it
         $msg = new RODSMessage();
